8c73afc115e01da372ed784f7e391d7d70f1755d
khyperia <953151+khyperia@users.noreply.github.com>
2024-06-27 14:23:27 +0200

    Add override to Cuchulainn, Kraken, Rafflesia, Golem

--- src/WorldCSV.cs
+++ src/WorldCSV.cs
@@ -6,13 +6,15 @@ internal class WorldCSV
 {
     public JsonNode? ExportJson()
     {
-        if (string.IsNullOrWhiteSpace(Name) || !IsPublic)
+        if (string.IsNullOrWhiteSpace(Name) || !IsPublic && !OverrideIsPublic)
         {
             return null;
         }
 
         return Name;
     }
+
+    private bool OverrideIsPublic => Name is "Cuchulainn" or "Kraken" or "Rafflesia" or "Golem";
 
     public string? Name { get; set; }
     public bool IsPublic { get; set; }
